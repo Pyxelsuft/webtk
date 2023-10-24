acba1fac73fb2c3e3ffb124bbbe032a61ff1392d
Pixelsuft <yuskivl07@mail.ru>
2023-10-24 19:01:36 +0700

    upd

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import io
 import base64
+import threading
 import webtk
+import requests
 from PIL import ImageGrab
 
 
 class WebViewApp:
     def __init__(self) -> None:
-        self.wv = webtk.webview.create_webview(debug=False)
+        self.wv = webtk.webview.create_webview(debug=True)
         self.wv.set_min_size(320, 200)
         # self.wv.set_size(1024, 768)
         self.wv.bind('py_title', self.set_title_cb)
         self.wv.bind('py_print', self.print_text_cb)
         self.wv.bind('py_stop', self.stop_cb)
         self.wv.bind('py_screen', self.screen_cb)
+        self.wv.bind('py_fetch', lambda req_id: threading.Thread(target=self.fetch_cb, args=(req_id, )).start())
         self.wv.set_js_hook(open('test.js').read())
         self.wv.set_html(open('test.html').read().replace('test.css here please', open('test.css').read()))
         self.wv.run()
+
+    def fetch_cb(self, req_id: bytes) -> None:
+        try:
+            json_data = requests.get('https://api.github.com/users/pixelsuft').json()
+            resp_data = {
+                'fws': json_data['followers'],
+                'fwg': json_data['following'],
+                'rps': json_data['public_repos']
+            }
+        except Exception as _err:
+            print(f'Failed to fetch github stats ({_err})')
+            resp_data = {
+                'fws': -1,
+                'fwg': -1,
+                'rps': -1
+            }
+        self.wv.response(req_id, resp_data)
 
     def stop_cb(self, req_id: bytes) -> None:
         self.wv.response(req_id)
--- test.js
+++ test.js
@@ -7,9 +7,6 @@ window.onload = function() {
             setTimeout(title_change, temp_title[0] == 'P' ? 3000 : 200);
         });
     }
-    title_change();
-
-    py_print('Hello', 'world');
 
     document.addEventListener('keydown', function(e) {
         if (e.keyCode == 27 || e.keyCode == 81)  // ESCape or Q
@@ -22,7 +19,22 @@ window.onload = function() {
         });
     });
 
+    document.getElementById('button1').addEventListener('click', function(e) {
+        py_fetch().then(function(e) {
+            console.log(e, e.followers);
+            document.getElementById('text0').textContent = `Pixelsuft: ${e.fws} followers, ${e.fwg} following, ${e.rps} repositories`;
+        });
+    });
+
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
     });
+
+    document.getElementById('screen0').addEventListener('dragstart', function(e) {
+        e.preventDefault();
+    });
+
+    title_change();
+
+    py_print('Hello', 'world');
 }
