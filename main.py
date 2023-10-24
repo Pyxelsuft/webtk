e34da5347eef168bf9c8f2751e062618e2178727
Pixelsuft <yuskivl07@mail.ru>
2023-10-24 18:42:43 +0700

    upd

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import io
+import base64
 import webtk
+from PIL import ImageGrab
 
 
 class WebViewApp:
     def __init__(self) -> None:
-        self.wv = webtk.webview.create_webview(debug=True)
+        self.wv = webtk.webview.create_webview(debug=False)
         self.wv.set_min_size(320, 200)
         # self.wv.set_size(1024, 768)
         self.wv.bind('py_title', self.set_title_cb)
         self.wv.bind('py_print', self.print_text_cb)
+        self.wv.bind('py_stop', self.stop_cb)
+        self.wv.bind('py_screen', self.screen_cb)
         self.wv.set_js_hook(open('test.js').read())
-        self.wv.set_html(open('test.html').read())
+        self.wv.set_html(open('test.html').read().replace('test.css here please', open('test.css').read()))
         self.wv.run()
+
+    def stop_cb(self, req_id: bytes) -> None:
+        self.wv.response(req_id)
+        self.wv.stop()
+
+    def screen_cb(self, req_id: bytes) -> None:
+        buf = io.BytesIO()
+        img = ImageGrab.grab()
+        img.save(buf, 'png')
+        buf.seek(0)
+        self.wv.response(req_id, b'["data:image/png;base64,' + base64.b64encode(buf.read()) + b'"]')
 
     def set_title_cb(self, req_id: bytes, new_title: str) -> None:
         self.wv.set_title(new_title)
--- test.js
+++ test.js
@@ -4,10 +4,25 @@ window.onload = function() {
     function title_change() {
         temp_title = temp_title.substr(1) + temp_title[0];
         py_title(temp_title).then(function() {
-            setTimeout(title_change, temp_title[0] == 'P' ? 3000 : 500);
+            setTimeout(title_change, temp_title[0] == 'P' ? 3000 : 200);
         });
     }
     title_change();
 
     py_print('Hello', 'world');
+
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 27 || e.keyCode == 81)  // ESCape or Q
+            py_stop();
+    });
+
+    document.getElementById('button0').addEventListener('click', function(e) {
+        py_screen().then(function(e) {
+            document.getElementById('screen0').src = e[0];
+        });
+    });
+
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
 }
